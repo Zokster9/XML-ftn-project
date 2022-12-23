44ab1583f205180e62d0c93c64b0181e5bbe26e7
ColdBubbyGuy <sakal.teodor@gmail.com>
2022-12-24 00:59:42 +0100

    bugfix

--- xml-project/src/main/java/project/xmlproject/transformXML/PatentTransformation.java
+++ xml-project/src/main/java/project/xmlproject/transformXML/PatentTransformation.java
@@ -4,6 +4,14 @@ import com.itextpdf.html2pdf.HtmlConverter;
 import com.itextpdf.kernel.geom.PageSize;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.update.UpdateExecutionFactory;
+import org.apache.jena.update.UpdateFactory;
+import org.apache.jena.update.UpdateProcessor;
+import org.apache.jena.update.UpdateRequest;
+import project.xmlproject.Util.AuthenticationUtilities;
+import project.xmlproject.Util.ConnectionUtilities;
 import project.xmlproject.XmlProjectApplication;
 import project.xmlproject.dto.creationDto.ZahtevZaPriznanjePatentaCreationDto;
 import project.xmlproject.marshal.MarshalPatent;
@@ -36,9 +44,19 @@ public class PatentTransformation {
     public static final String XSL_FILE = "/xslt/patent.xsl";
     public static final String XSL_TO_RDF_FILE = "/xslt/metadata.xsl";
 
+    private static final String METADATA_GRAPH_URI = "/metadata";
+
     //public static final String HTML_FILE = "src/main/resources/project/xmlproject/html/patent.html";
 
     public static final String OUTPUT_FILE = "src/main/java/project/xmlproject/pdf/patent.pdf";
+
+    public static String dropAll() {
+        return "DROP ALL";
+    }
+
+    public static String insertData(String graphURI, String ntriples) {
+        return String.format("INSERT DATA { GRAPH <%1$s> { %2$s } }", graphURI, ntriples);
+    }
 
     static {
 
@@ -59,7 +77,7 @@ public class PatentTransformation {
         HtmlConverter.convertToPdf(Files.newInputStream(Paths.get(html)), pdfDocument);
     }
 
-    /**public void generateRDF() {
+    public void generateRDF(String rdfFile, ZahtevZaPriznanjePatentaCreationDto zahtevZaPriznanjePatentaCreationDto) {
         try {
             TransformerFactory factory = TransformerFactory.newInstance();
             InputStream resourceAsStream = getClass().getResourceAsStream(XSL_TO_RDF_FILE);
@@ -71,7 +89,45 @@ public class PatentTransformation {
             ZahtevZaPriznanjePatenta zahtevZaPriznanjePatenta = marshalPatent.marshalPatent(zahtevZaPriznanjePatentaCreationDto, "read");
             JAXBSource source = new JAXBSource(context, zahtevZaPriznanjePatenta);
             System.out.println("Source" + source);
-            StreamResult result = new StreamResult(new FileOutputStream(htmlFile));
+            StreamResult result = new StreamResult(new FileOutputStream(rdfFile));
+
+            transformer.transform(source, result);
+
+            ConnectionUtilities.ConnectionProperties conn = ConnectionUtilities.loadProperties();
+
+            // Creates a default model
+            Model model = ModelFactory.createDefaultModel();
+            //TODO Ne pronalazi putanju
+            model.read(XSL_TO_RDF_FILE);
+
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+            model.write(out, "N-TRIPLES");
+
+            System.out.println("[INFO] Rendering model as RDF/XML...");
+            model.write(System.out, "RDF/XML");
+
+            // Delete all of the triples in all of the named graphs
+            UpdateRequest request = UpdateFactory.create() ;
+            request.add(dropAll());
+
+            /*
+             * Create UpdateProcessor, an instance of execution of an UpdateRequest.
+             * UpdateProcessor sends update request to a remote SPARQL update service.
+             */
+            UpdateProcessor processor = UpdateExecutionFactory.createRemote(request, conn.updateEndpoint);
+            processor.execute();
+
+            // Creating the first named graph and updating it with RDF data
+            System.out.println("[INFO] Writing the triples to a named graph \"" + METADATA_GRAPH_URI + "\".");
+            String sparqlUpdate = insertData(conn.dataEndpoint + METADATA_GRAPH_URI, new String(out.toByteArray()));
+            System.out.println(sparqlUpdate);
+
+            // UpdateRequest represents a unit of execution
+            UpdateRequest update = UpdateFactory.create(sparqlUpdate);
+
+            processor = UpdateExecutionFactory.createRemote(update, conn.updateEndpoint);
+            processor.execute();
 
 
         } catch (TransformerConfigurationException e) {
@@ -80,8 +136,10 @@ public class PatentTransformation {
             throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
+        } catch (TransformerException e) {
+            throw new RuntimeException(e);
         }
-    }**/
+    }
 
     public void generateHTML(String htmlFile, ZahtevZaPriznanjePatentaCreationDto zahtevZaPriznanjePatentaCreationDto) {
         //actimem.com/java/xslt-with-jaxb
@@ -106,7 +164,43 @@ public class PatentTransformation {
             StreamSource xslt1 = new StreamSource(resourceAsStream1);
             Transformer transformer1 = factory1.newTransformer(xslt1);
             StreamResult result1 = new StreamResult(new FileOutputStream("src/main/resources/rdf/neki.rdf"));
+
             transformer1.transform(source, result1);
+
+            ConnectionUtilities.ConnectionProperties conn = ConnectionUtilities.loadProperties();
+
+            // Creates a default model
+            Model model = ModelFactory.createDefaultModel();
+            model.read(XSL_TO_RDF_FILE);
+
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+            model.write(out, "N-TRIPLES");
+
+            System.out.println("[INFO] Rendering model as RDF/XML...");
+            model.write(System.out, "RDF/XML");
+
+            // Delete all of the triples in all of the named graphs
+            UpdateRequest request = UpdateFactory.create() ;
+            request.add(dropAll());
+
+            /*
+             * Create UpdateProcessor, an instance of execution of an UpdateRequest.
+             * UpdateProcessor sends update request to a remote SPARQL update service.
+             */
+            UpdateProcessor processor = UpdateExecutionFactory.createRemote(request, conn.updateEndpoint);
+            processor.execute();
+
+            // Creating the first named graph and updating it with RDF data
+            System.out.println("[INFO] Writing the triples to a named graph \"" + METADATA_GRAPH_URI + "\".");
+            String sparqlUpdate = insertData(conn.dataEndpoint + METADATA_GRAPH_URI, new String(out.toByteArray()));
+            System.out.println(sparqlUpdate);
+
+            // UpdateRequest represents a unit of execution
+            UpdateRequest update = UpdateFactory.create(sparqlUpdate);
+
+            processor = UpdateExecutionFactory.createRemote(update, conn.updateEndpoint);
+            processor.execute();
 
         } catch (TransformerConfigurationException e) {
             e.printStackTrace();
--- xml-project/src/main/resources/xslt/metadata.xsl
+++ xml-project/src/main/resources/xslt/metadata.xsl
@@ -6,7 +6,7 @@
 
     <xsl:template match="/">
         <rdf:RDF>
-            <rdf:Description rdf:about="http://www.ftn.uns.ac.rs.com/patent">
+            <rdf:Description rdf:about="Patent">
                 <xsl:variable name="Broj_prijave">
                     <xsl:value-of select="//Nova_prijava/Broj_prijave"/>
                 </xsl:variable>
@@ -16,14 +16,32 @@
                 <xsl:variable name="Priznati_datum_prijave">
                     <xsl:value-of select="//Nova_prijava/Priznati_datum_prijave"/>
                 </xsl:variable>
-                <rdf:Description rdf:about="http:/www.ftn.uns.ac.rs/Broj_prijave">
+                <xsl:variable name="Srpski_naziv_pronalaska">
+                    <xsl:value-of select="//Naziv_pronalaska/Srpski_naziv"/>
+                </xsl:variable>
+                <xsl:variable name="Engleski_naziv_pronalaska">
+                    <xsl:value-of select="//Naziv_pronalaska/Engleski_naziv"/>
+                </xsl:variable>
+                <xsl:variable name="Naziv_podnosioca">
+                    <xsl:value-of select="//Podnosilac/Naziv"/>
+                </xsl:variable>
+                <rdf:Description rdf:about="Broj_prijave">
                     <xsl:value-of select="$Broj_prijave"/>
                 </rdf:Description>
-                <rdf:Description rdf:about="http:/www.ftn.uns.ac.rs/Datum_prijave">
+                <rdf:Description rdf:about="Datum_prijave">
                     <xsl:value-of select="$Datum_prijave"/>
                 </rdf:Description>
-                <rdf:Description rdf:about="http:/www.ftn.uns.ac.rs.rs/Priznati_datum_prijave">
+                <rdf:Description rdf:about="Priznati_datum_prijave">
                     <xsl:value-of select="$Priznati_datum_prijave"/>
+                </rdf:Description>
+                <rdf:Description rdf:about="Srpski_naziv_pronalaska">
+                    <xsl:value-of select="$Srpski_naziv_pronalaska"/>
+                </rdf:Description>
+                <rdf:Description rdf:about="Engleski_naziv_pronalaska">
+                    <xsl:value-of select="$Engleski_naziv_pronalaska"/>
+                </rdf:Description>
+                <rdf:Description rdf:about="Naziv_podnosioca">
+                    <xsl:value-of select="$Naziv_podnosioca"/>
                 </rdf:Description>
             </rdf:Description>
         </rdf:RDF>
